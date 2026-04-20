7e979cca4773bb7c985244515f88fc50d27652ec
Braun Alexander Maximilian Ralph <alexander.braun@itsincom.it>
2026-04-20 11:44:45 +0200

    Creazione entità

---
+++ Autousate/Data/Cliente.cs
@@ -0,0 +1,22 @@
+﻿namespace Autousate.Data
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateOnly DatadiNascita { get; set; }
+        public string LuogodiNascita { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string NumeroCartaIdentita { get; set; }
+        public string IndirizzodiResidenza { get; set; }
+        public string NumeroCellulare { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Auto Auto;
+        public Concessionaria Concessionaria { get; set; }
+
+    }
+}
