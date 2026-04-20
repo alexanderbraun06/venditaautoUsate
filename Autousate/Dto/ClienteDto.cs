b125828223793fa8b0457af4df8eeb0128f25071
Braun Alexander Maximilian Ralph <alexander.braun@itsincom.it>
2026-04-20 12:46:42 +0200

    Creazione_DTO

--- Autousate/Dto/ClienteDto.cs
+++ Autousate/Dto/ClienteDto.cs
@@ -1,23 +1,19 @@
-﻿namespace Autousate.Data
+﻿using Autousate.Data;
+
+namespace Autousate.Dto
 {
-    public class Cliente
+    public class ClienteDto
     {
+        public int IdAuto { get; set; }
         public int Id { get; set; }
         public int IdCliente { get; set; }
         public string Nome { get; set; }
         public string Cognome { get; set; }
         public DateOnly DatadiNascita { get; set; }
         public string LuogodiNascita { get; set; }
-        public string CodiceFiscale { get; set; }
-        public string NumeroCartaIdentita { get; set; }
-        public string IndirizzodiResidenza { get; set; }
         public string NumeroCellulare { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int IdAuto {  get; set; }
-        public List<Auto> Auto;
-        public Concessionaria Concessionaria { get; set; }
-       
 
     }
 }
---
+++ Autousate/Dto/Mapper.cs
@@ -0,0 +1,85 @@
+﻿using Autousate.Data;
+using Autousate.Dto;
+
+namespace Autousate.Dto {
+    public class Mapper
+    {
+        public ClienteDto MapBaseEntityToDto(Cliente entity)
+        {
+            ClienteDto dto = new ClienteDto()
+            {
+                IdAuto = entity.IdAuto,
+                IdCliente = entity.IdCliente,
+                Nome = entity.Nome,
+                Cognome = entity.Cognome,
+                DatadiNascita = entity.DatadiNascita,
+                NumeroCellulare = entity.NumeroCellulare,
+                Email = entity.Email,
+                Password = entity.Password,
+
+            };
+            return dto;
+        }
+        public ClienteDto MapEntityToDto(Cliente entity)
+        {
+            ClienteDto dto = MapBaseEntityToDto(entity);
+
+            return dto;
+        }
+
+        public Cliente MapDtoToEntity(ClienteDto dto)
+        {
+            Cliente entity = new Cliente()
+            {
+                IdAuto = dto.IdAuto,
+                IdCliente = dto.IdCliente,
+                Nome = dto.Nome,
+                Cognome = dto.Cognome,
+                DatadiNascita = dto.DatadiNascita,
+                NumeroCellulare = dto.NumeroCellulare,
+                Email = dto.Email,
+                Password = dto.Password,
+
+            };
+            return entity;
+        }
+
+        public AutoDto MapAutoBaseEntityToDto(Auto entity)
+        {
+            AutoDto dto = new AutoDto()
+            {
+                IdAuto = entity.IdAuto,
+                IdCliente = entity.IdCliente,
+                Produttore = entity.Produttore,
+                Modello = entity.Modello,
+                DataPrimaImmatricolazione = entity.DataPrimaImmatricolazione,
+                NumeroKm = entity.NumeroKm,
+                Prezzo = entity.Prezzo,
+            };
+            return dto;
+        }
+
+        public AutoDto MapEntityToDto(Auto entity)
+        {
+            AutoDto dto = MapAutoBaseEntityToDto(entity);
+
+            return dto;
+        }
+        public ConcessionariaDto MapCaBaseEntityToDto(Concessionaria entity)
+        {
+            ConcessionariaDto dto = new ConcessionariaDto()
+            {
+                IdConcessionaria = entity.IdConcessionaria,
+                Nome = entity.Nome,
+            };
+            return dto;
+        }
+        public ConcessionariaDto MapCaEntityToDto(Concessionaria entity)
+        {
+            ConcessionariaDto dto = MapCaBaseEntityToDto(entity);
+            return dto;
+        }
+    
+    }
+        }
+        
